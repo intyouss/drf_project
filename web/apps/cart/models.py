288fb23d84de393aedb25eca26e57258b368c64d
intyouss <intyouss@github.com>
2023-05-28 11:39:35 +0800

    add: 添加购物车商品表模型

--- web/apps/cart/models.py
+++ web/apps/cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from common.default_model import BaseModel
+
+
+class Cart(BaseModel):
+    """购物车商品表"""
+    user = models.ForeignKey('users.Users', verbose_name='用户ID', on_delete=models.CASCADE, blank=True)
+    goods = models.ForeignKey('goods.Goods', verbose_name='商品ID', on_delete=models.CASCADE)
+    number = models.SmallIntegerField(verbose_name='商品数量', default=1, blank=True)
+    is_checked = models.BooleanField(verbose_name='是否选中', default=True, blank=True)
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = '购物车'
+        verbose_name_plural = verbose_name
