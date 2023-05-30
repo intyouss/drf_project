64223c511a6b4424c32c8ffd32e2726c38355983
intyouss <intyouss@github.com>
2023-05-30 14:18:00 +0800

    add: 添加订单模型后台管理

--- web/apps/order/admin.py
+++ web/apps/order/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderGoods, OrderCommit
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'order_number', 'amount', 'address', 'pay_type', 'status']
+
+
+@admin.register(OrderGoods)
+class OrderGoodsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'goods', 'price', 'number']
+
+
+@admin.register(OrderCommit)
+class OrderCommitAdmin(admin.ModelAdmin):
+    list_display = ['user', 'goods', 'order', 'content', 'star', 'rate']
