57422f1abd02bb11d2c8cffd3c2fd4d731b7d688
intyouss <123916296+intyouss@users.noreply.github.com>
2023-06-06 18:32:24 +0800

    fix: 测试

--- web/web/settings.py
+++ web/web/settings.py
@@ -4,7 +4,7 @@ import time
 from datetime import timedelta
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
