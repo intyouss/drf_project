6097acc640d130d46627807dd98daecfd8892d09
intyouss <123916296+intyouss@users.noreply.github.com>
2023-06-07 13:45:09 +0800

    add: 添加celery配置管理任务调度

--- web/web/settings.py
+++ web/web/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'corsheaders',
     # 富文本编辑器
     'ckeditor',
+    # 任务调度
+    'django_celery_beat',
     # app
     'users',
     'goods',
@@ -237,6 +239,22 @@ CACHES = {
             'CONNECTION_POOL_KWARGS': {'max_connections': 100, 'decode_responses': True},
         },
     },
+    'celery_broker': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/4',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'CONNECTION_POOL_KWARGS': {'max_connections': 100},
+        },
+    },
+    'celery_result': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/5',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'CONNECTION_POOL_KWARGS': {'max_connections': 100},
+        },
+    },
 }
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'session'
@@ -284,6 +302,15 @@ LOGGING = {
         },
     },
 }
+
+# celery 任务调度
+CELERY_BROKER_URL = 'redis://localhost:6379/4'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/5'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Shanghai'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 try:
     from .local_settings import *
